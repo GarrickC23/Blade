72e437bbe4bc63eae344ce1c11e929853dd37d36
Garrick Chiu <74120169+GarrickC23@users.noreply.github.com>
2023-02-22 13:51:37 -0800

    Organized Folders and Started Health System

---
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; 
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+   [SerializeField]
+   private float maxHealth;
+   [SerializeField]
+   private float Health;
+   [HideInInspector]
+   public bool isParrying = false;
+
+   public Image[] hearts; 
+   public Sprite fullHeart;
+   public Sprite emptyHeart; 
+
+   private void Start() {
+      Health = maxHealth;
+   }
+
+   public void TakeDamage(float damage)
+   {
+      Health -= damage;
+      for ( int i = 0; i < hearts.Length; i++ )
+      {
+         if ( i < Health )
+         {
+            hearts[i].sprite = fullHeart;
+         }
+         else
+         {
+            hearts[i].sprite = emptyHeart; 
+         }
+         if (Health <= 0){
+            Destroy(gameObject);
+         }
+      }
+      if (Health <= 0)
+      {
+         Destroy(gameObject);
+      }
+   }
+}
