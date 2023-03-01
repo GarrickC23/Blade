5b5a156ab76e350314b351853664dbab23f2eefb
Ethan <66433242+YoungsterEthan@users.noreply.github.com>
2023-02-28 23:03:14 -0800

    Removed Dash Release. Can dash if not moving

--- Assets/Scripts/Player/Movement/Dashing.cs
+++ Assets/Scripts/Player/Movement/Dashing.cs
@@ -17,14 +17,11 @@ public class Dashing : MonoBehaviour
     private bool isGrounded = true; // assume player starts on the ground
     private float currentDashTime = 0f;
     private float currentDashCooldown = 0f;
-    public float dashDeceleration;
-
 
     private void Awake()
     {
         playerControls = new PlayerControls();
         playerControls.Ground.Dash.performed += Dash;
-        playerControls.Ground.Dash.canceled += DashRelease;
     }
 
     private void OnEnable()
@@ -42,7 +39,8 @@ public class Dashing : MonoBehaviour
         if (isDashing)
         {
             // Move horizontally at a constant speed during the dash
-           
+            rb.velocity = new Vector2(dashSpeed * transform.right.x, 0);
+
             currentDashTime -= Time.deltaTime;
             if (currentDashTime <= 0f)
             {
@@ -58,53 +56,13 @@ public class Dashing : MonoBehaviour
 
     private void Dash(InputAction.CallbackContext context)
     {
-        if (context.performed && !isDashing && currentDashCooldown <= 0f)
+        if (context.performed && currentDashCooldown <= 0f)
         {
             Debug.Log("Dash!");
             isDashing = true;
             currentDashTime = dashTime;
-            Debug.Log(currentDashTime);
-
-            // Calculate deceleration rate based on remaining dash time
-            dashDeceleration = dashSpeed / currentDashTime;
-
-            StartCoroutine(Dash());
         }
     }
-
-    private IEnumerator Dash()
-    {
-        float originalGravityScale = rb.gravityScale;
-        rb.gravityScale = 0f;
-
-        while (currentDashTime > 0f)
-        {
-            yield return null;
-
-            // Calculate new velocity based on dash speed and deceleration
-            float deceleration = dashDeceleration * Time.deltaTime;
-            float speed = Mathf.Max(0f, dashSpeed - deceleration);
-            rb.velocity = new Vector2(speed * rb.velocity.x, rb.velocity.y);
-
-            currentDashTime -= Time.deltaTime;
-
-            if (!playerControls.Ground.Dash.ReadValue<float>().Equals(1f)) // dash button is not held down
-            {
-                isDashing = false;
-                break;
-            }
-        }
-
-        rb.gravityScale = originalGravityScale;
-    }
-
-    private void DashRelease(InputAction.CallbackContext context)
-    {
-        rb.velocity = new Vector2(0, rb.velocity.y);
-    }
-
 }
-
-
 
 
