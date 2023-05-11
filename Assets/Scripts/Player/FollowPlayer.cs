ac8f28b04acbbabb110eb7560e7fc2b9b665ffbe
myong003 <matthew.yong0801@gmail.com>
2023-05-11 13:43:19 -0700

    basic screen shake

--- Assets/Scripts/Player/FollowPlayer.cs
+++ Assets/Scripts/Player/FollowPlayer.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject player; 
+    private GameObject player;
+    private bool isFrozen;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerPosition = player.transform.position;
-        this.transform.position = new Vector3(playerPosition.x, playerPosition.y + 8, -10);
+        if (!isFrozen) {
+            Vector3 playerPosition = player.transform.position;
+            this.transform.position = new Vector3(playerPosition.x, playerPosition.y + 8, -10);
+        }
+    }
+
+    public void FreezeFollow() {
+        isFrozen = true;
+    }
+
+    public void UnfreezeFollow() {
+        isFrozen = false;
     }
 }
