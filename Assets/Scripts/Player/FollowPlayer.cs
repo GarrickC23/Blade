935faaca354ac9b02964169f5405b871d515a17b
myong003 <matthew.yong0801@gmail.com>
2023-05-11 14:22:50 -0700

    Finished screen shake

--- Assets/Scripts/Player/FollowPlayer.cs
+++ Assets/Scripts/Player/FollowPlayer.cs
@@ -6,9 +6,16 @@ public class FollowPlayer : MonoBehaviour
 {
     private GameObject player;
     private bool isFrozen;
+    private bool isShaking;
+    private float currDuration;
+    private float currIntensity;
+    private float shakeTimer;
+    private float directionTimer;
+    private float direction;
 
     private void Start() {
         player = GameObject.FindGameObjectWithTag("Player");
+        // CallShakeScreen(1, 1);
     }
 
     // Update is called once per frame
@@ -18,6 +25,23 @@ public class FollowPlayer : MonoBehaviour
             Vector3 playerPosition = player.transform.position;
             this.transform.position = new Vector3(playerPosition.x, playerPosition.y + 8, -10);
         }
+        if (isShaking) {
+            if (shakeTimer < currDuration) {
+                this.gameObject.transform.position += new Vector3(direction * currIntensity, 0, 0);
+                if (directionTimer >= 0.05f) {   // 0.05f is the time between switching directions for shaking
+                    direction = -direction;
+                    directionTimer = 0;
+                }
+                else {
+                    directionTimer += Time.deltaTime;
+                }
+                shakeTimer += Time.deltaTime;
+            }
+            else {
+                isShaking = false;
+                shakeTimer = 0;
+            }
+        }
     }
 
     public void FreezeFollow() {
@@ -26,5 +50,12 @@ public class FollowPlayer : MonoBehaviour
 
     public void UnfreezeFollow() {
         isFrozen = false;
+    }
+
+    public void CallShakeScreen(float shakeTime, float intensity) {
+        currDuration = shakeTime;
+        currIntensity = intensity;
+        isShaking = true;
+        direction = 1;
     }
 }
